a0051626ea22de61c3b6a27a17f5f5c5f9a6ba5e
Your Name <your.email@example.com>
2025-11-19 11:55:20 +0100

    Simplify server.py

--- server.py
+++ server.py
@@ -1,30 +1,12 @@
-﻿from flask import Flask, render_template, jsonify
-import json
-import os
+﻿import os
+from flask import Flask
 
-app = Flask(__name__, template_folder='templates', static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
-def index():
-    # Wczytaj listę przetworzonych książek
-    books = []
-    if os.path.exists('reader3_data'):
-        for folder in os.listdir('reader3_data'):
-            book_path = os.path.join('reader3_data', folder)
-            if os.path.isdir(book_path):
-                books.append(folder)
-    
-    return render_template('index.html', books=books)
-
-@app.route('/api/books')
-def get_books():
-    books = []
-    if os.path.exists('reader3_data'):
-        for folder in os.listdir('reader3_data'):
-            if os.path.isdir(os.path.join('reader3_data', folder)):
-                books.append(folder)
-    return jsonify({'books': books})
+def hello():
+    return 'Reader3 is running!'
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 8123))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
